824a117aaef2b7651b63216f859914a1a65bed6d
Kaan Kaya <kaankaya0743@gmail.com>
2024-10-23 17:15:38 +0300

    Temel entityler oluşturuldu

---
+++ Core/SiteApi.Domain/Entities/Brand.cs
@@ -0,0 +1,26 @@
+﻿using SiteApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteApi.Domain.Entities
+{
+    public class Brand :EntityBase
+    {
+        public Brand()
+        {
+            
+        }
+        public Brand(string name)       // nesne oluştururken zorunlu alanları bu ctor sayesinde görcez kolaylık sağlıcak
+        {
+            Name = name;
+        }
+
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
---
+++ Core/SiteApi.Domain/Entities/Category.cs
@@ -0,0 +1,39 @@
+﻿using SiteApi.Domain.Common;
+using SiteApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteApi.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category()
+        {
+            
+        }
+        public Category(int parentId, string name, int priorty)   // nesne oluştururken zorunlu alanları bu ctor sayesinde görcez kolaylık sağlıcak
+        {
+            Priorty = priorty;
+            ParentId = parentId;
+            Name = name;
+        }
+
+        [Required]
+        public int ParentId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Priorty { get; set; }
+
+        public ICollection<Detail> Details { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
+
