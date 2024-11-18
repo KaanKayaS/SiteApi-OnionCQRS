21acc2c993cb4b9f112c6106c0211fbf4045a6ff
Kaan Kaya <kaankaya0743@gmail.com>
2024-11-18 14:22:11 +0300

    Token service yapılıd , create token metodu yapılandırıldı.

---
+++ Infrastructure/SiteApi.Infrastructure/Registration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SiteApi.Infrastructure.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteApi.Mapper
+{
+    public static class Registration
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+           
+        }
+    }
+}
