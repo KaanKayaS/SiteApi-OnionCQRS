cdb2aaa5bf21f843b27b71d75ebd73f986e65fef
Kaan Kaya <kaankaya0743@gmail.com>
2024-11-20 15:29:40 +0300

    Revoke ve revokeall işlemleri yapıldı

---
+++ Core/SiteApi.Application/Features/Auth/Command/RevokeAll/RevokeAllCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using SiteApi.Application.Bases;
+using SiteApi.Application.Interfaces.AutoMapper;
+using SiteApi.Application.Interfaces.UnitOfWorks;
+using SiteApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace SiteApi.Application.Features.Auth.Command.RevokeAll
+{
+    public class RevokeAllCommandHandler : BaseHandler, IRequestHandler<RevokeAllCommandRequest, Unit>
+    {
+        private readonly UserManager<User> userManager;
+
+        public RevokeAllCommandHandler(UserManager<User> userManager,IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<Unit> Handle(RevokeAllCommandRequest request, CancellationToken cancellationToken)
+        {
+            List<User> users = userManager.Users.ToList();
+
+            foreach (User user in users)
+            {
+                user.RefreshToken = null;
+                await userManager.UpdateAsync(user);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
